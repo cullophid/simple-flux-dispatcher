5b6fd9d5301c4117bd688c206d177c823547ef56
Andreas Møller <andreas.moller@gmail.com>
2015-07-01 01:18:50 +0100

    update

--- scripts/setVersion.js
+++
@@ -1,7 +0,0 @@
-var json = require('../package.json')
-var fs = require('fs');
-var path = require('path');
-var version = json.version.split('.');
-version.pop();
-json.version = version.concat([process.env.CIRCLE_BUILD_NUM]).join('.')
-fs.writeFileSync(path.join(__dirname, '../package.json'), JSON.stringify(json, null, 2));
